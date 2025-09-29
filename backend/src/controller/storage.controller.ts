598656952fbb9dadc43f3923bdb247ccc90f5ae1
Arsh <arsil.m@hashtechy.com>
2025-09-29 23:16:23 +0530

    78 done

---
+++ backend/src/controller/storage.controller.ts
@@ -0,0 +1,71 @@
+import { GetObjectCommand, HeadObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import { Request, Response } from "express"
+import { CatchError, TryError } from "../util/errors"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+
+const connection = new S3Client({
+    region: process.env.REGION,
+    endpoint: `https://s3-${process.env.REGION}.amazonaws.com`,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+    }
+})
+
+const isFileExist = async (path: string) => {
+    try {
+        const command = new HeadObjectCommand({
+            Bucket: process.env.S3_NETWORK,
+            Key: path
+        })
+        await connection.send(command)
+        return true
+    } catch (error) {
+        return false
+    }
+}
+
+export const downloadFile = async (req: Request, res: Response) => {
+    try {
+        const path = req?.body?.path
+
+        if (!path || path === undefined)
+            throw TryError("Path is missing", 400)
+
+        const checkFileExist = await isFileExist(path)
+        if (!checkFileExist)
+            throw TryError("File doesn't exists!", 404)
+
+        const option = {
+            Bucket: process.env.S3_NETWORK,
+            Key: path
+        }
+
+        const command = new GetObjectCommand(option)
+        const url = await getSignedUrl(connection, command, { expiresIn: 60 })
+        res.json({ url })
+    } catch (error) {
+        CatchError(error, res, "Failed to generate download url")
+    }
+}
+
+export const uploadFile = async (req: Request, res: Response) => {
+    try {
+        const path = req?.body?.path
+        const type = req?.body?.type
+
+        if (!path || !type)
+            throw TryError("Invalid request", 400)
+
+        const command = new PutObjectCommand({
+            Bucket: process.env.S3_NETWORK,
+            Key: path,
+            ContentType: type
+        })
+
+        const url = await getSignedUrl(connection, command, { expiresIn: 60 })
+        res.json({ url })
+    } catch (error) {
+        CatchError(error, res, "Failed to generate upload url")
+    }
+}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,24 +3,26 @@ dotenv.config()
 
 import mongoose from 'mongoose'
 mongoose.connect(process.env.DB!).then(() => console.log('Database connected'))
-    .catch(() => console.log('Database not connected'))
+  .catch(() => console.log('Database not connected'))
 
 import express from 'express'
 import cors from 'cors'
 import AuthRouter from "./router/auth.router"
 import cookieParser from 'cookie-parser'
+import StorageRouter from './router/storage.router'
 
 const app = express()
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`Server is running on ${process.env.PORT}`)
+  console.log(`Server is running on ${process.env.PORT}`)
 })
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "http://localhost:5173",
   credentials: true, // allow cookies
 }))
 
 app.use("/auth", AuthRouter)
+app.use("/storage", StorageRouter)
