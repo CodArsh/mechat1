a07f84a71d1509c9fee65f9a5560b3be21eff18e
Arsh <arsil.m@hashtechy.com>
2025-09-29 21:42:30 +0530

    catch error

--- src/lib/CatchError.ts
+++ src/lib/CatchError.ts
@@ -1,11 +1,14 @@
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import axios from "axios"
+import { toast, type ToastPosition } from "react-toastify"
 
-const CatchError = (err:unknown) => {
-    if (axios.isAxiosError(err))
-        return toast.error(err.response?.data?.message)
-    if (err instanceof Error)
-        return toast.error(err.message)
+const CatchError = (err: unknown, position: ToastPosition= "top-center") => {
+    if(axios.isAxiosError(err))
+    return toast.error(err.response?.data.message, {position})
+
+    if(err instanceof Error)
+    return toast.error(err.message, {position})
+
+    return toast.error("Network error", {position})
 }
 
 export default CatchError
